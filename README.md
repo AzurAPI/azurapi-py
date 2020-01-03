8a3c2108fd69167b2280634e7601a9b330aec1ac
August (Chris) <augu@voided.pw>
2020-01-03 13:56:51 -0700

    Add setup file and README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Azur Lane API
+[![Discord](https://discordapp.com/api/guilds/648206344729526272/embed.png)](https://discord.gg/aAEdys8)
+
+Repository for the Python library for the unofficial Azur Lane API
+
+## Example
+```py
+from azurapi import AzurApi
+
+api = AzurApi()
+api.get_ship(name="Enterprise")
+```
+
+## Maintainers
+- [August](https://github.com/auguwu)
+- [Spimy](https://github.com/Spimy)
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+# Gets the requirements
+def get_requirements():
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+
+    return requirements
+
+# The version number
+version = '1.0.0'
+
+# README
+readme = ''
+
+# Finds the "README.md" file
+# and reads it
+with open('README.md') as file:
+    readme = file.read()
+
+# Setups for Pypi
+setup(
+    name='azurapi',
+    packages=['azurapi'],
+    version=version,
+    description='Unofficial Azur Lane API library made in Python',
+    long_description=str(readme),
+    author='August',
+    author_email='ohlookitsaugust@gmail.com',
+    url='https://github.com/AzurAPI/azurapi-py',
+    include_package_data=True,
+    keywords=['azurlane', 'azur lane'],
+    install_requires=get_requirements(),
+    classifiers=[
+        'Proramming Language :: Python :: 3.6'
+    ]
+)
