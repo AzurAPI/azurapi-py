e2b9b137448f631a4a00db44546a4774573c5fd5
August (Chris) <augu@voided.pw>
2020-01-05 12:13:08 -0700

    Merge get_chapter and get_chapter_diff, update code to be readable

--- azurapi.py
+++ azurapi.py
@@ -121,32 +121,30 @@ class AzurAPI:
         
         return ship[0]
 
-    # Not honestly needed but
-    # more user friendly
-    def get_all_chapters(self):
-        return self.chapter_list
+    def get_chapter(self, chapter, **kwargs):
 
-    def get_chapter(self, code):
-        if "-" not in code:
+        # Check if "-" is in the chapter argument
+        if "-" not in chapter:
             raise ValueError('The chapter code must be padded as "1-1"')
 
-        chapter = code.split("-")
-        if not self.chapter_list[chapter[0]][chapter[1]]:
-            raise exceptions.UnknownChapterException(f"Unknown chapter: {chapter[0]}-{chapter[1]}")
+        # Split the chapter
+        [c, s] = chapter.split("-")
+
+        # Python makes these an integer for some reason
+        chap = str(c)
+        stage = str(s)
+        diff = kwargs.get("diff", None)
+
+        if not self.chapter_list[chap][stage]:
+            raise exceptions.UnknownChapterException(f"Unknown chapter: {chap}-{stage}")
+        elif diff is not None:
+            if not self.chapter_list[chap][stage][diff]:
+                raise exceptions.UnknownDifficultyException(f"Unknown difficulty: {chap}-{stage} ({diff})")
+            else:
+                return self.chapter_list[chap][stage][diff]
         else:
-            return self.chapter_list[chapter[0]][chapter[1]]
-
-    def get_chapter_diff(self, code, diff):
-        if "-" not in code:
-            raise ValueError('The chapter code must be padded as "1-1"')
-
-        chapter = code.split("-")
-        if not self.chapter_list[chapter[0]][chapter[1]]:
-            raise exceptions.UnknownChapterException(f"Unknown chapter: {chapter[0]}-{chapter[1]}")
-        elif not self.chapter_list[chapter[0]][chapter[1]][diff]:
-            raise exceptions.UnknownDifficultyException(f"Unknown chapter difficulty: {diff}")
-
-        return self.chapter_list[chapter[0]][chapter[1]][diff]
+            return self.chapter_list[chap][stage]
+                
 
 if __name__ == "__main__":
     azurapi = AzurAPI()
--- test/chapter_test.py
+++ test/chapter_test.py
@@ -2,7 +2,7 @@ import azurapi as api
 azur = api.AzurAPI()
 
 try:
-    # print(azur.get_chapter(code='1-1'))
-    print(azur.get_chapter_diff(code='1-1', diff='normal'))
+    # print(azur.get_chapter(chapter='1-1'))
+    print(azur.get_chapter(chapter='1-1', diff='hard'))
 except ValueError as e:
     print(f"{type(e).__name__}: {e}")
