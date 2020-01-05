0b9919b64792b5d2b929dddd0bea777bc2df1b7a
August (Chris) <augu@voided.pw>
2020-01-05 12:15:59 -0700

    Double quote strings in setup.py

--- setup.py
+++ setup.py
@@ -2,36 +2,36 @@ from setuptools import setup
 
 # Gets the requirements
 def get_requirements():
-    with open('requirements.txt') as f:
+    with open("requirements.txt") as f:
         requirements = f.read().splitlines()
 
     return requirements
 
 # The version number
-version = '1.0.0'
+version = "1.0.0"
 
 # README
-readme = ''
+readme = ""
 
 # Finds the "README.md" file
 # and reads it
-with open('README.md') as file:
+with open("README.md") as file:
     readme = file.read()
 
 # Setups for Pypi
 setup(
-    name='azurapi',
-    packages=['azurapi'],
+    name="azurapi",
+    packages=["azurapi"],
     version=version,
-    description='Unofficial Azur Lane API library made in Python',
+    description="Unofficial Azur Lane API library made in Python",
     long_description=str(readme),
-    author='August',
-    author_email='ohlookitsaugust@gmail.com',
-    url='https://github.com/AzurAPI/azurapi-py',
+    author="August",
+    author_email="ohlookitsaugust@gmail.com",
+    url="https://github.com/AzurAPI/azurapi-py",
     include_package_data=True,
-    keywords=['azurlane', 'azur lane'],
+    keywords=["azurlane", "azur lane"],
     install_requires=get_requirements(),
     classifiers=[
-        'Programming Language :: Python :: 3.7'
+        "Programming Language :: Python :: 3.7"
     ]
 )
