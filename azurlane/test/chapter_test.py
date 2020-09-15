2d4a491c38b6109a7f1d271f9ea05d909347f892
Spimy <officialspyme@gmail.com>
2020-09-15 20:52:12 +0400

    Remove v1 files

--- azurlane/test/chapter_test.py
+++
@@ -1,8 +0,0 @@
-from ..azurapi import AzurAPI
-azur = AzurAPI()
-
-try:
-    # print(azur.get_chapter(chapter='1-1'))
-    print(azur.get_chapter(chapter='1-1', diff='hard'))
-except ValueError as e:
-    print(f"{type(e).__name__}: {e}")
